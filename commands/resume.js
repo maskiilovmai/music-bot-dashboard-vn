c520d37b1e9705bafcb94eef35b81802bbf73d05
maskiilovmai <maikimlong2405@gmail.com>
2021-11-02 05:25:42 +0700

    main

--- commands/resume.js
+++ commands/resume.js
@@ -3,7 +3,7 @@ const { TrackUtils } = require("erela.js");
 
 module.exports = {
   name: "resume",
-  description: "Resumes the music",
+  description: "Tiếp tục bài hát",
   usage: "",
   permissions: {
     channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
